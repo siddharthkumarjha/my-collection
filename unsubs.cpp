e4119a113ce490a77cb82cd48a3779432abc7434
Siddharth Kumar Jha <siddharthkjha6909@gmail.com>
2023-09-18 20:39:36 +0530

    Added prog to count unique substrings

---
+++ unsubs.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int len;
+  string s;
+  cin >> len;
+  cin >> s;
+  int count=0;
+  for(int i=0; i<=len-4; i++){
+    string temp = s.substr(i, 4);
+    //cout<<i<<"_"<<temp<<" ";
+    map<char, int>m;
+    int irq=0;
+    for(int j=0; j<4; j++)m[temp[j]]++;
+    for(auto it : m){
+      if(it.second > 1){
+        irq = 1;
+        break;
+      }
+    }
+    if(!irq) count++;
+  }
+  cout<<count<<endl;
+  return 0;
+}
